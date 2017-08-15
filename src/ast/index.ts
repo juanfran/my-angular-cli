278198b8aeac8c4e999ca967a1cd4c645906ca34
juanfran <juanfran.ag@gmail.com>
2017-08-15 10:51:04 +0200

    find ast node

--- src/ast/index.ts
+++ src/ast/index.ts
@@ -7,44 +7,89 @@ function lineChar(sourceFile, node) {
   console.log(line, character);
 }
 
+function find(node: ts.Node, kind: ts.SyntaxKind, text?: string): ts.Node | undefined {
+  for(let children of node.getChildren()) {
+    if (children.kind === kind && (!text || (text && text === children.getText()))) {
+      return children;
+    } else {
+      const childrenFinded = find(children, kind, text);
+
+      if (childrenFinded) {
+        return childrenFinded;
+      }
+    }
+  }
+}
+
 function compile(fileNames: string[], options: ts.CompilerOptions): void {
   fileNames.forEach(fileName => {
 
-      let sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES5, true);
+      let sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.Latest, true);
       //console.log(sourceFile);
 
+      const module = find(sourceFile, ts.SyntaxKind.Decorator);
+
+      if (module && module.parent) {
+        //console.log(module);
+        /*let x = module.getChildAt(1).getChildAt(2).getChildAt(0).getChildAt(1);
+        console.log(x.getChildAt(0));
+        console.log(x.getChildAt(0).getText());*/
+
+        const imports = find(module, ts.SyntaxKind.Identifier, 'imports');
+
+        if (imports && imports.parent) {
+          console.log(imports.parent);
+          console.log(imports.parent.getChildAt(2).getText());
+          console.log(imports.getText());
+        }
+
+        //module = find(sourceFile, ts.SyntaxKind.Identifier, 'NgModule');
+
+        //console.log(module.getText());
+      }
+      /*
       ts.forEachChild(sourceFile, (x) => {
         if (x.kind === ts.SyntaxKind.ClassDeclaration) {
-          console.log('-----each');
-          console.log(x.decorators);
-          const printer = ts.createPrinter({
-              newLine: ts.NewLineKind.LineFeed,
-          });
-
-          const result = printer.printNode(ts.EmitHint.Unspecified, x, sourceFile);
+          console.log('-----each');;
 
           console.log('==========================');
           //console.log(result);
 
           if (x.decorators && x.decorators.length) {
-            const expression = x.decorators[0].expression;
-            expression.getChildren().forEach((c, index) => {
-              //console.log(index + '-->', c.getText());
-            });
+            // NgModule
+            let x1 = x.getChildAt(0).getChildAt(0).getChildAt(1).getChildAt(0);
+            console.log('x1', x1.kind, x1.getText());
+            console.log(x1);
 
+            const expression = x.decorators[0].expression;
 
             const initDecoData = expression.getChildAt(2).getChildAt(0).getChildAt(1);
             const imports = initDecoData.getChildAt(0);
             const listImports = imports.getChildAt(2);
-            console.log(listImports.getChildAt(1)); // browser module
-    /*        if (expression.) {
-              console.log(x.decorators[0].expression.);
-            }*/
+            //console.log(listImports.getChildAt(1).getText()); // browser module
+
+
+
           }
         }
-      });
+      });*/
   });
 }
+
+
+/*            function addImport(file, name, pathTo) {
+	file.statements.unshift(
+		ts.createImportDeclaration(
+			undefined,
+			undefined,
+			ts.createImportClause(ts.createIdentifier(name)),
+			ts.createLiteral(pathTo)
+		)
+	);
+	return file;
+}
+
+*/
 
 export const exampleCompile = () => {
   compile([path.join(__dirname, 'example.ts')], {
