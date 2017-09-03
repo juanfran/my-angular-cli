3ed87886c8cae0ddd2daeb90a295386dc8ab66bc
juanfran <juanfran.ag@gmail.com>
2017-09-03 19:15:40 +0200

    add test specs

---
+++ src/ast/test.spec.ts
@@ -0,0 +1,179 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai'
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe('Ast', () => {
+  describe('add component', () => {
+    it('add component in inline declaration', () => {
+      const file = `
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [ AppComponent, ExammpleComponent ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+
+      const fileExpected = `
+      import { TestComponent } from ../component1/testcomponent';
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [ AppComponent, ExammpleComponent, TestComponent ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+    });
+
+    it('add component in inline declaration with trailing comma', () => {
+      const file = `
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [ AppComponent, ExammpleComponent, ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+
+      const fileExpected = `
+      import { TestComponent } from ../component1/testcomponent';
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [ AppComponent, ExammpleComponent, TestComponent ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+    });
+
+    it('add component in multiline declaration', () => {
+      const file = `
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [
+          AppComponent,
+          ExammpleComponent
+        ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+
+      const fileExpected = `
+      import { TestComponent } from ../component1/testcomponent';
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [
+          AppComponent,
+          ExammpleComponent,
+          TestComponent
+        ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+    });
+
+    it('add component in multiline declaration with trailing comma', () => {
+      const file = `
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [
+          AppComponent,
+          ExammpleComponent,
+        ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+
+      const fileExpected = `
+      import { TestComponent } from ../component1/testcomponent';
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [
+          AppComponent,
+          ExammpleComponent,
+          TestComponent
+        ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+    });
+
+    it('add component without declarations', () => {
+      const file = `
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+
+      const fileExpected = `
+      import { TestComponent } from ../component1/testcomponent';
+      import { NgModule }      from '@angular/core';
+      import { AppComponent }  from './app.component';
+
+      @NgModule({
+        imports: [
+          TestModule
+        ],
+        declarations: [ TestComponent ],
+        bootstrap: [ AppComponent ]
+      })
+      export class AppModule { }
+      `;
+    });
+  });
+
+});
